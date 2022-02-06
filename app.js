0dbded2c3176b34d768a042c99824c648a8391d5
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-02-05 23:08:27 -0500

    introduccion a express

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-const http=require('http');
+const express = require("express");
+require("dotenv").config();
 
+const app=express();
+const port=process.env.PORT || 3000;
+app.get('/', (req, res)=>{
+  res.send('Hola kisama')
+})
+app.get('/hola', (req, res)=>{
+  res.send('Hola saitama')
+})
+app.get('*', (req, res)=>{
+  // comodin para las pagians que no eisten
+  res.send('Error de pagina, no existe !!!')
+})
 
-http.createServer((req, res) => {
-  // res.writeHead(200,{'Content-Type': 'application/json'});
-
-  res.setHeader('Content-Disposition', 'attachment; filename="lista.csv"')
-  res.writeHead(200,{'Content-Type': 'application/csv'});
-  const persona={
-    id:2,
-    name:'saitama'
-  }
-  // res.write(JSON.stringify(persona));
-  res.write('id; nombre\n');
-  res.write('1; Saitama\n');
-  res.write('2; genos\n');
-
-  
-  res.end()
-}).listen(8080)
-
+app.listen(port, ()=>{
+  console.log(`Escuchando en el puerto ${port} en http://localhost:${port}`);
+})
---
+++ app_old.js
@@ -0,0 +1,21 @@
+const http=require('http');
+
+
+http.createServer((req, res) => {
+  // res.writeHead(200,{'Content-Type': 'application/json'});
+
+  // res.setHeader('Content-Disposition', 'attachment; filename="lista.csv"')
+  // res.writeHead(200,{'Content-Type': 'application/csv'});
+  // const persona={
+  //   id:2,
+  //   name:'saitama'
+  // }
+  // // res.write(JSON.stringify(persona));
+  // res.write('id; nombre\n');
+  // res.write('1; Saitama\n');
+  res.write('Hols mundo');
+
+  
+  res.end()
+}).listen(8080)
+
