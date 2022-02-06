c4c8cb548c3b0f760c7ce903d0795a392b8098fd
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-02-05 22:29:04 -0500

    escribiendo en el navegados  status contenttype para ver la data

--- app.js
+++ app.js
@@ -2,7 +2,20 @@ const http=require('http');
 
 
 http.createServer((req, res) => {
-  res.write('Hola mundo');
+  // res.writeHead(200,{'Content-Type': 'application/json'});
+
+  res.setHeader('Content-Disposition', 'attachment; filename="lista.csv"')
+  res.writeHead(200,{'Content-Type': 'application/csv'});
+  const persona={
+    id:2,
+    name:'saitama'
+  }
+  // res.write(JSON.stringify(persona));
+  res.write('id; nombre\n');
+  res.write('1; Saitama\n');
+  res.write('2; genos\n');
+
+  
   res.end()
 }).listen(8080)
 
