b7e17ff6619e951a87e2c660c7d88580fddca28c
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-02-06 00:30:30 -0500

    partials y renderizandos las otras paginas

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const express = require("express");
-require("dotenv").config();
 const port=process.env.PORT || 3000;
 const app=express();
-
+const hbs=require("hbs");
+require("dotenv").config();
+// handlebars
+// partials
+hbs.registerPartials(__dirname + "/views/partials")
 // renderizar handlebars
 app.set('view engine', 'hbs');
 //servir contenido estatico
 app.use(express.static('public'))
 
 
-
 app.get('/', (req, res)=>{
   res.render('home',{nombre:'Saitama','age':25,titulo:"Curso de Node"})
 })
 app.get('/elements', (req, res)=>{
-  res.sendFile(__dirname +'/public/elements.html')
+  res.render('elements',{nombre:'Kakaroto','age':25,titulo:"Curso de HBS"})
 })
 app.get('/generic', (req, res)=>{
-  res.sendFile(__dirname +'/public/generic.html')
+  res.render('generic',{nombre:'Genos','age':25,titulo:"Curso de Node HBS"})
 })
 app.get('*', (req, res)=>{
   // comodin para las pagians que no eisten
